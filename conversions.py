57754989d7567a2295dcec4ce608cf764cde129a
chrisharris000 <chris2145.ch@gmail.com>
2024-07-14 20:12:02 +1000

    added type hinting to function

--- conversions.py
+++ conversions.py
@@ -2,16 +2,16 @@ import datetime
 
 import mgrs
 
-def _convert_mgrs_to_lat_lon(mgrs_coord):
+def _convert_mgrs_to_lat_lon(mgrs_coord: str):
     m = mgrs.MGRS()
     return m.toLatLon(mgrs_coord)
 
-def _convert_lat_lon_to_mgrs(lat_coord, lon_coord):
+def _convert_lat_lon_to_mgrs(lat_coord: float, lon_coord: float):
     m = mgrs.MGRS()
     c = m.toMGRS(lat_coord, lon_coord)
     return c
 
-def convert_mgrs_lat_lon(mgrs_coord, lat_coord, lon_coord):
+def convert_mgrs_lat_lon(mgrs_coord: str, lat_coord: str, lon_coord: str):
     if not mgrs_coord:
         mgrs_coord_output = _convert_lat_lon_to_mgrs(float(lat_coord), float(lon_coord))
         lat_coord_output = lat_coord
@@ -28,29 +28,29 @@ def convert_mgrs_lat_lon(mgrs_coord, lat_coord, lon_coord):
     }
     return coordinate_vars
 
-def _convert_ft_to_m(feet):
-    return float(feet) / 3.281
+def _convert_ft_to_m(feet: float):
+    return feet / 3.281
 
-def _convert_m_to_ft(metres):
-    return float(metres) * 3.281
+def _convert_m_to_ft(metres: float):
+    return metres * 3.281
 
-def convert_m_ft(metres, feet):
+def convert_m_ft(metres: str, feet: str):
     if not metres:
-        metres_output = _convert_ft_to_m(feet)
+        metres_output = _convert_ft_to_m(float(feet))
         feet_output = feet
     else:
-        feet_output = _convert_m_to_ft(metres)
+        feet_output = _convert_m_to_ft(float(metres))
         metres_output = metres
-    
+
     distance_vars = {
         "metres_output": metres_output,
         "feet_output": feet_output
     }
     return distance_vars
 
-def _convert_epoch_to_date(epoch_time):
+def _convert_epoch_to_date(epoch_time: int):
     try:
-        dt = datetime.datetime.fromtimestamp(int(epoch_time))
+        dt = datetime.datetime.fromtimestamp(epoch_time)
         date_time = dt.strftime('%Y-%m-%d %H:%M:%S')
     except ValueError:
         date_time = "Invalid input format"
@@ -58,7 +58,7 @@ def _convert_epoch_to_date(epoch_time):
         date_time = f"Error: {e}"
     return date_time
 
-def _convert_date_to_epoch(date_time):
+def _convert_date_to_epoch(date_time: str):
     try:
         dt = datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
         epoch_time = int(dt.timestamp())
@@ -68,33 +68,33 @@ def _convert_date_to_epoch(date_time):
         epoch_time = f"Error: {e}"
     return epoch_time
 
-def convert_epoch_date(epoch_time, date_time):
+def convert_epoch_date(epoch_time: str, date_time: str):
     if not date_time:
-        date_time_output = _convert_epoch_to_date(epoch_time)
+        date_time_output = _convert_epoch_to_date(int(epoch_time))
         epoch_time_output = epoch_time
     else:
         epoch_time_output = _convert_date_to_epoch(date_time)
         date_time_output = date_time
-    
+
     distance_vars = {
         "date_time_output": date_time_output,
         "epoch_time_output": epoch_time_output
     }
     return distance_vars
 
-def _convert_knots_to_mps(knots):
+def _convert_knots_to_mps(knots: float):
     return knots * 0.5144
 
-def _convert_mps_to_knots(mps):
+def _convert_mps_to_knots(mps: float):
     return mps * 1.943844
 
-def _convert_mps_to_kmh(mps):
+def _convert_mps_to_kmh(mps: float):
     return mps / 1000 * 60 * 60
 
-def _convert_kmh_to_mps(kmh):
+def _convert_kmh_to_mps(kmh: float):
     return kmh * 1000 / 60 / 60
 
-def convert_knots_mps_kmh(knots, mps, kmh):
+def convert_knots_mps_kmh(knots: str, mps: str, kmh: str):
     if not knots and not mps:
         mps_output = _convert_kmh_to_mps(float(kmh))
         knots_output = _convert_mps_to_kmh(float(mps_output))
